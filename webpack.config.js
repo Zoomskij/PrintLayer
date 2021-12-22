834786650df08f5319fc150882ce6c6777e364cc
apotapov <a.potapov@1001tur.ru>
2021-12-22 13:10:52 +0300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+﻿const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    devtool: 'source-map',
+    entry: "./wwwroot/src/js/app.js",
+    output: {
+        path: path.resolve(__dirname, "wwwroot/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: true
+                        }
+                    },
+
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
