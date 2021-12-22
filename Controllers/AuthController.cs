8a9155e24e4daa47e2ba49d527c4a1471e849bdc
apotapov <a.potapov@1001tur.ru>
2021-12-22 18:18:30 +0300

    authorization /auth/login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using PrintLayer.Models;
+using PrintLayer.Services.Interfaces;
+
+namespace PrintLayer.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        [AllowAnonymous]
+        public async Task<User> Login([FromBody] LoginModel credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                //return BadRequest();
+            }
+
+           // if (HttpContext.User != null)
+           //     await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var user = await _authService.LoginAsync(credentials.Login, credentials.Password);
+
+            //var user = await _authService.CreateUserAsync(credentials.Login, credentials.Password);
+
+            if (user == null) return null;
+
+            var nameIdentifier = user.Id.ToString();
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+            return user;
+
+            //return SignIn(claimsPrincipal, new AuthenticationProperties
+            //    {
+            //        ExpiresUtc = DateTime.UtcNow.AddDays(1),
+            //        IsPersistent = false,
+            //        IssuedUtc = DateTime.UtcNow
+            //    }
+            //    , CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult Logout()
+        {
+            if (HttpContext.User == null)
+                return BadRequest();
+
+            return SignOut(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
--- wwwroot/src/js/app.js
+++ wwwroot/src/js/app.js
@@ -3,15 +3,47 @@
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale'
 import ruLocale from 'element-ui/lib/locale/lang/ru-RU'
+import axios from 'axios';
+
+import { Input } from 'element-ui'
 
 locale.use(ruLocale);
 Vue.use(ElementUI, { ruLocale });
+Vue.prototype.$axios = axios;
+
+Vue.use(Input);
 
 function startOnLoad() {
     var app = new Vue({
         el: '#app',
         data: {
-            message: 'Vue!'
+            message: 'Vue!',
+            login: '',
+            password: '',
+            user:[]
+        },
+        methods: {
+            auth: function () {
+                //console.log(this.login);
+                var self = this;
+                var model = {
+                    login: this.login,
+                    password: this.password
+                }
+                this.$axios.post('/auth/login', model, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(function (response) {
+                        self.user = response.data;
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        self.user = error;
+                        console.log(error);
+                    });
+            }
         }
     });
 }
