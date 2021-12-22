bb3c3fab877e4b1f0f6aaaa254185b6d944eeb55
apotapov <a.potapov@1001tur.ru>
2021-12-22 14:58:37 +0300

    added elemen-ui

--- wwwroot/src/js/app.js
+++ wwwroot/src/js/app.js
@@ -1,4 +1,11 @@
-﻿
+﻿import Vue from 'vue'
+
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale'
+import ruLocale from 'element-ui/lib/locale/lang/ru-RU'
+
+locale.use(ruLocale);
+Vue.use(ElementUI, { ruLocale });
 
 function startOnLoad() {
     var app = new Vue({
