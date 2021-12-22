dab72c3a66ddf78a47040ab148369e41d6f72beb
apotapov <a.potapov@1001tur.ru>
2021-12-22 14:39:14 +0300

    added generic repository/service

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using PrintLayer.Models;
 
 namespace PrintLayer.Data
 {
     public class Context : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
         public Context(DbContextOptions<Context> options) : base(options)
         {
             Database.EnsureCreated();
